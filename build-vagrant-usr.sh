9db0f07dc61a65d5d54a3535c519a28871a2b5ff
Cameron Murphy <camurphy@gmail.com>
2019-01-09 00:37:07 +1100

    Disable progress bar on nvm install

--- build-vagrant-usr.sh
+++ build-vagrant-usr.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install --lts
+nvm install --lts --no-progress
 nvm alias default stable
 npm install npm@latest -g
 
