dce52924fa3b942f33eb878db224dfeede8ee651
Cameron Murphy <camurphy@gmail.com>
2019-01-08 21:45:33 +1100

    Bump nvm

--- build-vagrant-usr.sh
+++ build-vagrant-usr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
