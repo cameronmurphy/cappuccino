ae9f4082c4f15c07b4bcd01a1beb34129ce5fdcb
Cameron Murphy <camurphy@gmail.com>
2019-11-07 12:38:13 +1100

    Remove comments

--- build-vagrant-usr.sh
+++ build-vagrant-usr.sh
@@ -3,8 +3,8 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"
 
 nvm install --lts --no-progress
 nvm alias default stable
