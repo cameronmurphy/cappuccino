90482f58e4edff14542907d967c4d17b237c4578
Cameron Murphy <camurphy@gmail.com>
2020-12-01 13:52:20 +1100

    Upgrade to NVM 0.37.2

--- build-vagrant-usr.sh
+++ build-vagrant-usr.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"
-[[ -s "$NVM_DIR/bash_completion" ]] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install --lts --no-progress
 nvm alias default stable
